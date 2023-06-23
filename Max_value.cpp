81a5bdcaeebe0f00d8ebd07de3625cb70ed408bc
Suprit <64748931+supritkhanke@users.noreply.github.com>
2023-06-23 15:02:13 +0530

    Add files via upload

---
+++ Max_value.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int getMax(int num[], int n) {
+    int max = INT16_MIN;
+
+    for(int i = 0; i<n; i++) {
+        if (num[i] > max) {
+            max = num[i];
+        }
+    }
+
+    // Return max value
+    return max;
+}
+
+int main() {
+    int size;
+    cin >> size;
+
+    int num[100];
+
+    // Taking input in array
+    for (int i = 0; i < size; i++) {
+        cin >> num[i];
+    }
+
+    cout << "Maximum value is " << getMax(num, size) << endl;
+
+    return 0;
+}
